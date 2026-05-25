f4d77549e36021d89bd58ab13efa3fccdb23e89b
nassaigael <hei.nassai@gmail.com>
2026-05-25 12:29:53 +0300

    style(client): improve ClientEquipmentCard design with better hover effects and layout

--- src/components/client/ClientEquipmentCard.tsx
+++ src/components/client/ClientEquipmentCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Package, Wifi, Calendar, Building2, Wrench } from 'lucide-react';
+import { Package, Wifi, Calendar, Building2 } from 'lucide-react';
 import ClientMaintenanceRequest from './ClientMaintenanceRequest';
 
 interface ClientEquipmentCardProps {
