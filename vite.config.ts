763e423a36ae3b2859c4f8ed52bb1296faae0097
nassaigael <hei.nassai@gmail.com>
2026-05-26 19:02:24 +0300

    chore(vite): add host option for network access

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [react(), tailwindcss()
-],
+  plugins: [react(), tailwindcss()],
   define: {
     global: 'globalThis',
   },
   server: {
+    host: '0.0.0.0',
     port: 5173,
     proxy: {
       '/api': {
